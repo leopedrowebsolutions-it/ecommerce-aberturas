210abc2b0107ad163eb218a4ec589b5accea6a25
Leonardo Sanchez <leonardosanchez119@gmail.com>
2026-04-28 22:17:38 -0300

    agrego funcionalidad logica e visual del carrito

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductList } from "../components/product-list/product-list";
+import { Cart } from "../components/cart/cart";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductList],
+  imports: [RouterOutlet, ProductList, Cart],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/components/cart/cart.ts
+++ src/components/cart/cart.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-
+import { CartService } from '../../app/services/cart';
+import { CommonModule } from '@angular/common'; // 👈 FALTA ESTO
+import { Product } from '../../app/models/product.interface';
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule], // 👈 Y ESTO
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
 export class Cart {
 
+  constructor(private cartService: CartService) { }
+
+  items: Product[] = [];
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
+  remove(index: number) {
+    this.cartService.removeItem(index);
+  }
+
+  clear() {
+    this.cartService.clearCart();
+  }
 }
